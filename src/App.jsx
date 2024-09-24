ec6149cd08c3936d0943e0b492f62993b12a4e23
Paulo Almeida <pauloalneto@outlook.com>
2024-09-23 21:18:43 -0300

    Build: included tailwind references

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ function App() {
 
   return (
     <>
-     <h1>Hello World</h1>
-     <h3>Building a TO-DO web application with React and Tailwind</h3>
+     <h1 className="text-3xl font-bold underline">Hello World</h1>
+     <h3 className="mt-3 text-1xl text-zinc-700">Building a TO-DO web application with React and Tailwind</h3>
     </>
   )
 }
